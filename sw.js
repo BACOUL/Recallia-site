cfa3386f685bce8f448b652d4814a5d6200894b0
BACOUL <102251510+BACOUL@users.noreply.github.com>
2025-08-15 23:46:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,17 @@
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('recallia-v1').then(c=>c.addAll(['./','./index.html'])));
+// Service Worker minimal – Recallia
+const CACHE = 'recallia-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.webmanifest',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
